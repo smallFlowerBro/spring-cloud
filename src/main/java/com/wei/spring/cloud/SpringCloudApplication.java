2112266949c74bbc0ad03b572709c6d629cae644
weiyongjian <1262405508@qq.com>
2021-09-05 15:06:35 +0800

    spring cloud demo 初始化

---
+++ src/main/java/com/wei/spring/cloud/SpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.wei.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudApplication.class, args);
+    }
+
+}
